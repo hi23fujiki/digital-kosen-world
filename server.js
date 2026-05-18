ceb1aa49752f16bfa71a46c2435fc8900ced50f1
藤木熊本 <hi23fujiki@kumamoto.kosen-ac.jp>
2026-05-18 09:51:56 +0900

    fix port for render

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const characterHistory = [];
 const MAX_CHARACTERS = 30;
